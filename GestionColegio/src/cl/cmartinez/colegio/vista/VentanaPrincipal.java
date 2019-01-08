8bdf0fb0b55da93f7e3a753f1b8ab720dbea8fb8
Carlos Martinez Sanchez <carlos.f.martinez.s@gmail.com>
2019-01-07 21:34:37 -0300

    BackgroundPanel
    BackgroundPanel

--- GestionColegio/src/cl/cmartinez/colegio/vista/VentanaPrincipal.java
+++ GestionColegio/src/cl/cmartinez/colegio/vista/VentanaPrincipal.java
@@ -5,7 +5,14 @@
  */
 package cl.cmartinez.colegio.vista;
 
+import cl.cmartinez.colegio.vista.panels.BackgroundPanel;
 import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JLabel;
@@ -16,15 +23,20 @@ import javax.swing.JLabel;
  */
 public class VentanaPrincipal extends JFrame
 {
-    public VentanaPrincipal ()
+    public VentanaPrincipal () throws IOException
     {
         super("Sistema de Gestión de Colegios");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
+        Image bgImagen = ImageIO.read(new File("resources/imagenes/bg/wp2224998.jpg"));
+        BackgroundPanel bgPanel = new BackgroundPanel(bgImagen, BackgroundPanel.SCALED);
+        
         Container cp = getContentPane();
-        cp.setLayout(null);
+        
+        cp.setLayout(new FlowLayout());
+        cp.add(bgPanel);
         JLabel label = new JLabel("Bienvenido!");
         label.setBounds(30, 30, 200, 30);
-        cp.add(label);
+        bgPanel.add(label);
     }
 }
